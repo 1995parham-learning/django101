15014648f6800e3ddd754bbc52d464ec8f33f3b6
Parham Alvani <parham.alvani@gmail.com>
2022-11-14 19:15:03 +0330

    feat: complete helloer model

--- hello/models.py
+++ hello/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 
 class Helloer(models.Model):
-    Name = models.CharField(max_legnth=255)
+    name = models.CharField(max_legnth=255)
+    family = models.CharField(max_length=255)
+    age = models.IntegerField()
