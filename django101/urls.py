f6acac2d618f5759cbdbcff5ba62005f591848bc
Parham Alvani <parham.alvani@gmail.com>
2022-11-14 21:29:24 +0330

    feat: register apis app

--- django101/urls.py
+++ django101/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("hello", include("hello.urls")),
+    path("hello/", include("hello.urls")),
+    path("api/", include("apis.urls")),
 ]
