a81fef39e0168db495f084478d3aa81a0c8ab026
Parham Alvani <parham.alvani@gmail.com>
2022-11-14 19:15:54 +0330

    feat: add hello model migration

--- hello/models.py
+++ hello/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Helloer(models.Model):
-    name = models.CharField(max_legnth=255)
+    name = models.CharField(max_length=255)
     family = models.CharField(max_length=255)
     age = models.IntegerField()
