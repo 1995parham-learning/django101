6d94c87f703c5ad7305b9484e30eba805c73b46f
Parham Alvani <parham.alvani@gmail.com>
2022-11-25 19:12:05 +0330

    feat: add doc for helloer model

--- hello/models.py
+++ hello/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Helloer(models.Model):
+    """
+    Helloers are the persons that wants to say hello
+    """
+
     name = models.CharField(max_length=255)
     family = models.CharField(max_length=255)
     age = models.IntegerField()
