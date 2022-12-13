412fedc06fddeb0672b3a8727c6a60f1964674c6
Parham Alvani <parham.alvani@gmail.com>
2022-12-13 09:50:09 +0330

    feat: add a blog application

---
+++ blog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.DRAFT
+    )
+
+    class Meta:
+        ordering = ["-publish"]
+        indexes = [models.Index(fields=["-publish"])]
+
+    def __str__(self):
+        return self.title
