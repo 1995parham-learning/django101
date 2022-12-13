e2cc3b983ce520cf6a61aae668f082db2384b7d3
Parham Alvani <parham.alvani@gmail.com>
2022-12-13 13:36:12 +0330

    feat: change post url to have date and slug

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,9 @@ app_name = "blog"
 
 urlpatterns = [
     path("", views.post_list, name="post_list"),
-    path("<int:pk>/", views.post_detail, name="post_detail"),
+    path(
+        "<int:year>/<int:month>/<int:day>/<slug:post>",
+        views.post_detail,
+        name="post_detail",
+    ),
 ]
