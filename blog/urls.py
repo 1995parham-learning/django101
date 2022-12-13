682445c417c469787ce8eae44a22e1ef839ecc59
Parham Alvani <parham.alvani@gmail.com>
2022-12-13 17:17:43 +0330

    feat: use class-based view

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path("", views.post_list, name="post_list"),
+    path("", views.PostListView.as_view(), name="post_list"),
     path(
         "<int:year>/<int:month>/<int:day>/<slug:post_slug>",
         views.post_detail,
