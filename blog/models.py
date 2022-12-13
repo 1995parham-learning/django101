cdd094b8ac6a704a7023fe328b3a84121b53da0d
Parham Alvani <parham.alvani@gmail.com>
2022-12-13 12:07:59 +0330

    feat: make publish unique for a date

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
 
     id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
     body = models.TextField()
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
