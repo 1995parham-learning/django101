20ec1b8b9230d1998a4d4bbbef494b77abd665bd
Parham Alvani <parham.alvani@gmail.com>
2022-12-13 11:35:21 +0330

    feat: register urls for views

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,9 @@
+from django.urls import path
+from . import views
+
 app_name = "blog"
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.post_list, name="post_list"),
+    path("<int:id>/", views.post_detail, name="post_detail"),
+]
