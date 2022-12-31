18f62744fe90aef2b2e21b7b2f2717fa66b1908b
Parham Alvani <parham.alvani@gmail.com>
2022-12-31 20:32:01 +0330

    feat: add a route for sharing post via email

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from django.http.request import HttpRequest
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.views import generic
+from django.core.mail import send_mail
 from .models import Post
+from .forms import EmailPostForm
 
 
 class PostListView(generic.ListView):
@@ -23,3 +25,29 @@ def post_detail(
         publish__day=day,
     )
     return render(request, "blog/post/detail.html", {"post": post})
+
+
+def post_share(request: HttpRequest, post_id: int):
+    post = get_object_or_404(
+        Post.objects,
+        id=post_id,
+        status=Post.Status.PUBLISHED,
+    )
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+
+            send_mail(subject, message, "parham.alvani@gmail.com", [cd["to"]])
+    else:
+        form = EmailPostForm()
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form}
+    )
