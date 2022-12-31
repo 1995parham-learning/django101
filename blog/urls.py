1cb9e216455e5e1d3b377cc5dfe744bbacda1bba
Parham Alvani <parham.alvani@gmail.com>
2022-12-31 20:43:50 +0330

    feat: register url for post_share

--- blog/urls.py
+++ blog/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
         views.post_detail,
         name="post_detail",
     ),
+    path(
+        "<int:post_id>/share/",
+        views.post_share,
+        name="post_share",
+    ),
 ]
